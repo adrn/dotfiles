4593161dc08e91c465e3648e1b983f218d318062
Adrian Price-Whelan <adrian.prw@gmail.com>
2019-06-28 14:24:40 -0400

    mac defaults

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -1,11 +1,7 @@
-# Hijacked from @holman and @breadandbutter on github.
-#
-# This shell script sets some useful OS X defaults.
-#
+# This shell script set OS X defaults
 
-###############################################################################
-# General UI/UX                                                               #
-###############################################################################
+# Ask for sudo up front
+sudo -v
 
 # Set computer name (as done via System Preferences → Sharing)
 sudo scutil --set ComputerName "adrn"
@@ -13,15 +9,14 @@ sudo scutil --set HostName "adrn"
 sudo scutil --set LocalHostName "adrn"
 sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "adrn"
 
-# Disable press-and-hold for keys in favor of key repeat (10.8)
-#defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
-
-###############################################################################
-# Finder
-###############################################################################
+# Finder: show all filename extensions
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
 
 # Finder: show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
+
+# Finder: show path bar
+defaults write com.apple.finder ShowPathbar -bool true
 
 # Finder: allow text selection in Quick Look
 defaults write com.apple.finder QLEnableTextSelection -bool true
@@ -29,28 +24,36 @@ defaults write com.apple.finder QLEnableTextSelection -bool true
 # Display full POSIX path as Finder window title
 defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
 
-# Use list view in all Finder windows by default
-# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
-
 # Enable AirDrop over Ethernet and on unsupported Macs running Lion
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces -bool true
 
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
-###############################################################################                                 
-# Mail
-###############################################################################    
+# Use list view in all Finder windows by default
+# Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
 
-# Copy email addresses as `foo@example.com` instead of `Foo Bar <foo@example.com>` in Mail.app
-defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false
+# Automatically hide and show the Dock
+defaults write com.apple.dock autohide -bool true
 
-###############################################################################                                 
-# kill modified applications
-###############################################################################    
+# Save screenshots to the desktop
+defaults write com.apple.screencapture location -string "${HOME}/Desktop"
 
-for app in "Finder" "Mail" "SystemUIServer"; do
-    killall "$app" > /dev/null 2>&1
-done
+# Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
+defaults write com.apple.screencapture type -string "png"
+
+# Disable shadow in screenshots
+defaults write com.apple.screencapture disable-shadow -bool true
+
+# Avoid creating .DS_Store files on network or USB volumes
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+defaults write com.apple.desktopservices DSDontWriteUSBStores -bool true
+
+# Scroll direction
+defaults write com.apple.swipescrolldirection -bool false
+
+# Mouse tracking?
+defaults write com.apple.mouse.scaling -int 2
+
 echo "Done. Note that some of these changes require a logout/restart to take effect."
